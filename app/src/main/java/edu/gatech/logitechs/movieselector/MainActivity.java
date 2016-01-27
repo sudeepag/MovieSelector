ea28325741df4bc2c5e4a81f7d1fa1b2679d4dc9
jiangshen <jiangshen16@icloud.com>
2016-01-27 14:11:23 -0500

    minor changes on version

--- app/src/main/java/edu/gatech/logitechs/movieselector/MainActivity.java
+++ app/src/main/java/edu/gatech/logitechs/movieselector/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.gatech.logitechs.movieselector;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,6 +12,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog.setTitle("Alert");
+        alertDialog.setMessage("Hey whatsup");
+
+        alertDialog.show();
+
     }
 
     @Override
