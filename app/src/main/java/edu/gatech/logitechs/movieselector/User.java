f79ae12e7ee4a7dca46570309dfbd386629c1bd8
Akhilesh Aji <akhiaji@gmail.com>
2016-02-07 15:22:16 -0500

    Added User class with email and password

---
+++ app/src/main/java/edu/gatech/logitechs/movieselector/User.java
@@ -0,0 +1,15 @@
+package edu.gatech.logitechs.movieselector;
+
+/**
+ * Created by akhilesh on 2/7/16.
+ */
+public class User {
+    private String email;
+    private String password;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+        
+    }
+}
