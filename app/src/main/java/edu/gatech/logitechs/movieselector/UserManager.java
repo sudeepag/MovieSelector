6d6a8477673cc3ef154199ca927f14810e556ddd
MDozer3 <MDozer3@gatech.edu>
2016-02-07 17:20:15 -0500

    Added UserManager file with method to add new user and verify users at login

---
+++ app/src/main/java/edu/gatech/logitechs/movieselector/UserManager.java
@@ -0,0 +1,49 @@
+package edu.gatech.logitechs.movieselector;
+
+/**
+ * Created by matth_000 on 2/7/2016.
+ */
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserManager {
+
+    static Map<String, User> userList = new HashMap<>();
+
+    public UserManager() {
+        userList.put("Bob", new User("Bob", "1234"));
+    }
+    public Map<String, User> getUserList() {
+        return userList;
+    }
+
+    /**
+     * Method to add user to database of users
+     *
+     * @return true is email is unique and was added properly, false otherwise
+     */
+    public boolean addUser(User user) {
+        String newEmail = user.getEmail();
+        for (User curr : userList.values()) {
+            if (curr.getEmail().equals(newEmail)) {
+                return false;
+            }
+        }
+        userList.put(newEmail, user);
+        return true;
+    }
+
+    /**
+     * Authenticate users under the assumption that no duplacte usernames are
+     * allowed (enforced in the addUser method above)
+     *
+     * @return true if the input email and password match, false otherwise
+     */
+    public boolean authenticateUser(String email, String pass) {
+        User curr = userList.get(email);
+        if (curr == null) {
+            return false;
+        }
+        return curr.getPassword().equals(pass);
+    }
+}
