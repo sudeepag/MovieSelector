1951a047289b1a98f825646ff597f023e7485e3f
Akhilesh Aji <akhiaji@gmail.com>
2016-02-07 16:42:30 -0500

    Added getter and setter for each of the instance variables in user

--- app/src/main/java/edu/gatech/logitechs/movieselector/User.java
+++ app/src/main/java/edu/gatech/logitechs/movieselector/User.java
@@ -10,6 +10,22 @@ public class User {
     public User(String email, String password) {
         this.email = email;
         this.password = password;
-        
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
