f29e143215a7b6f498d7bede1b23426eae3f9f06
mloo3 <slayer71432@gmail.com>
2016-02-15 00:02:36 -0500

    Adding authentication with user manager and loading screen

--- app/src/main/java/edu/gatech/logitechs/movieselector/UserRegistration.java
+++ app/src/main/java/edu/gatech/logitechs/movieselector/UserRegistration.java
@@ -1,12 +1,18 @@
 package edu.gatech.logitechs.movieselector;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.app.LoaderManager;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Typeface;
 import android.os.Build;
+import android.provider.ContactsContract;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -14,14 +20,25 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.app.LoaderManager.LoaderCallbacks;
+
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
-public class UserRegistration extends AppCompatActivity {
+public class UserRegistration extends AppCompatActivity implements LoaderCallbacks<Cursor>{
     /**
      * Id to identity READ_CONTACTS permission request.
      */
@@ -30,6 +47,13 @@ public class UserRegistration extends AppCompatActivity {
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
     private EditText mConfirmPasswordView;
+    private View mProgressView;
+    private View mSignupFormView;
+
+    /**
+     * Keep track of the login task to ensure we can cancel it if requested.
+     */
+    private UserLoginTask mAuthTask = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,6 +103,9 @@ public class UserRegistration extends AppCompatActivity {
                 finish();
             }
         });
+
+        mSignupFormView = findViewById(R.id.signup_form);
+        mProgressView = findViewById(R.id.signup_progress);
     }
 
     /**
@@ -87,6 +114,9 @@ public class UserRegistration extends AppCompatActivity {
      * errors are presented and no actual login attempt is made.
      */
     private void attemptSignUp() {
+        if (mAuthTask != null) {
+            return;
+        }
         // Reset errors.
         mEmailView.setError(null);
         mPasswordView.setError(null);
@@ -138,26 +168,25 @@ public class UserRegistration extends AppCompatActivity {
             // There was an error; don't attempt login and focus the first
             // form field with an error.
             focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+
+            //Force bring down the keyboard
+            // Check if no view has focus:
+            View view = this.getCurrentFocus();
+            if (view != null) {
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            }
+
+            //transition to loading view
+            showProgress(true);
+
+            //start authentication
+            mAuthTask = new UserLoginTask(email, password);
+            mAuthTask.execute((Void) null);
         }
-//      else {
-//            // Show a progress spinner, and kick off a background task to
-//            // perform the user login attempt.
-//
-//            //Force bring down the keyboard
-//            // Check if no view has focus:
-//            View view = this.getCurrentFocus();
-//            if (view != null) {
-//                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-//                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-//            }
-//
-//            //transition to loading view
-//            showProgress(true);
-//
-//            //start authentication
-//            mAuthTask = new UserLoginTask(email, password);
-//            mAuthTask.execute((Void) null);
-//        }
     }
     private boolean isEmailValid(String email) {
         //TODO: Validate EMAIL
@@ -168,32 +197,159 @@ public class UserRegistration extends AppCompatActivity {
         return password.length() > 4;
     }
 
-   /* private void populateAutoComplete() {
-        if (!mayRequestContacts()) {
-            return;
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+
+        //isLoading = show;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_longAnimTime);
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+
+            mSignupFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mSignupFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mSignupFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mSignupFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
+   @Override
+   public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+       return new CursorLoader(this,
+               // Retrieve data rows for the device user's 'profile' contact.
+               Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
+                       ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
+
+               // Select only email addresses.
+               ContactsContract.Contacts.Data.MIMETYPE +
+                       " = ?", new String[]{ContactsContract.CommonDataKinds.Email
+               .CONTENT_ITEM_TYPE},
+
+               // Show primary email addresses first. Note that there won't be
+               // a primary email address if the user hasn't specified one.
+               ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
+   }
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+        List<String> emails = new ArrayList<>();
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            emails.add(cursor.getString(ProfileQuery.ADDRESS));
+            cursor.moveToNext();
         }
 
-        getLoaderManager().initLoader(0, null, getApplicationContext());
+        addEmailsToAutoComplete(emails);
     }
-    private boolean mayRequestContacts() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            return true;
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> cursorLoader) {
+
+    }
+
+    private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
+        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<>(UserRegistration.this,
+                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
+
+        mEmailView.setAdapter(adapter);
+    }
+
+
+    private interface ProfileQuery {
+        String[] PROJECTION = {
+                ContactsContract.CommonDataKinds.Email.ADDRESS,
+                ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
+        };
+
+        int ADDRESS = 0;
+        int IS_PRIMARY = 1;
+    }
+    /**
+     * Represents an asynchronous login/registration task used to authenticate
+     * the user.
+     */
+    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String mEmail;
+        private final String mPassword;
+
+        UserLoginTask(String email, String password) {
+            mEmail = email;
+            mPassword = password;
         }
-        if (checkSelfPermission(READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
-            return true;
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // TODO: attempt authentication against a network service.
+
+            try {
+                // Simulate network access.
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                return false;
+            }
+
+            UserManager manager = new UserManager();
+            return true;  //TODO HERE YOU MUST ADD THE USER AND AUTHENTICATE
+                            //IF THIS RETURNS TRUE, SUCESS = TRUE
+            //return manager.authenticateUser(mEmail, mPassword);
         }
-        if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
-            Snackbar.make(mEmailView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
-                    .setAction(android.R.string.ok, new View.OnClickListener() {
-                        @Override
-                        @TargetApi(Build.VERSION_CODES.M)
-                        public void onClick(View v) {
-                            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-                        }
-                    });
-        } else {
-            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
+
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            mAuthTask = null;
+            showProgress(false);
+
+            if (success) {
+                finish();
+                //go onto next activity if suceess
+                Intent myIntent = new Intent(UserRegistration.this,MainActivity.class);
+                UserRegistration.this.startActivity(myIntent);
+            } else {
+                new AlertDialog.Builder(UserRegistration.this)
+                        .setTitle(R.string.app_name)
+                        .setMessage(R.string.prompt_registration_error)
+                        .setPositiveButton("Okay",
+                                new DialogInterface.OnClickListener() {
+                                    @TargetApi(11)
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+                                    }
+                                })
+                        .show();
+                mEmailView.requestFocus();
+            }
         }
-        return false;
-    }*/
+
+        @Override
+        protected void onCancelled() {
+            mAuthTask = null;
+            showProgress(false);
+        }
+    }
 }
