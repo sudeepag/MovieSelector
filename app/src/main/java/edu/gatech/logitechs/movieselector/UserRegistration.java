840bfe807b77a442820df8629c7798a65d66a43d
jiangshen <jiangshen16@icloud.com>
2016-02-25 22:22:33 -0500

    clean up UserRegistration

--- app/src/main/java/edu/gatech/logitechs/movieselector/UserRegistration.java
+++ app/src/main/java/edu/gatech/logitechs/movieselector/UserRegistration.java
@@ -3,15 +3,12 @@ package edu.gatech.logitechs.movieselector;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.app.LoaderManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Build;
-import android.provider.ContactsContract;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -27,24 +24,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.app.LoaderManager.LoaderCallbacks;
 
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static android.Manifest.permission.READ_CONTACTS;
-
-public class UserRegistration extends AppCompatActivity implements LoaderCallbacks<Cursor>{
-    /**
-     * Id to identity READ_CONTACTS permission request.
-     */
-    //private static final int REQUEST_READ_CONTACTS = 0;
+public class UserRegistration extends AppCompatActivity{
 
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
@@ -118,7 +99,6 @@ public class UserRegistration extends AppCompatActivity implements LoaderCallbac
             public boolean onTouch(View v, MotionEvent event) {
                 // change the background color
                 switch (event.getAction() & MotionEvent.ACTION_MASK) {
-
                     case MotionEvent.ACTION_DOWN:
                         linkCancel.setTextColor(Color.rgb(189, 189, 189));
                         break;
@@ -244,8 +224,6 @@ public class UserRegistration extends AppCompatActivity implements LoaderCallbac
                     showProgress(false);
                 }
             });
-//            mAuthTask = new UserLoginTask(email, password, major, description);
-//            mAuthTask.execute((Void) null);
         }
     }
     private boolean isEmailValid(String email) {
@@ -271,7 +249,6 @@ public class UserRegistration extends AppCompatActivity implements LoaderCallbac
         // the progress spinner.
 
         //isLoading = show;
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
             int shortAnimTime = getResources().getInteger(android.R.integer.config_longAnimTime);
 
@@ -300,65 +277,8 @@ public class UserRegistration extends AppCompatActivity implements LoaderCallbac
         }
     }
 
-   @Override
-   public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-       return new CursorLoader(this,
-               // Retrieve data rows for the device user's 'profile' contact.
-               Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
-                       ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
-
-               // Select only email addresses.
-               ContactsContract.Contacts.Data.MIMETYPE +
-                       " = ?", new String[]{ContactsContract.CommonDataKinds.Email
-               .CONTENT_ITEM_TYPE},
-
-               // Show primary email addresses first. Note that there won't be
-               // a primary email address if the user hasn't specified one.
-               ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
-   }
-    @Override
-    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
-        List<String> emails = new ArrayList<>();
-        cursor.moveToFirst();
-        while (!cursor.isAfterLast()) {
-            emails.add(cursor.getString(ProfileQuery.ADDRESS));
-            cursor.moveToNext();
-        }
-
-        addEmailsToAutoComplete(emails);
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> cursorLoader) {
-
-    }
-
-    private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
-        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
-        ArrayAdapter<String> adapter =
-                new ArrayAdapter<>(UserRegistration.this,
-                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
-
-        mEmailView.setAdapter(adapter);
-    }
-
     public void transition() {
         Intent myIntent = new Intent(this,MainActivity.class);
         this.startActivity(myIntent);
     }
-
-    private interface ProfileQuery {
-        String[] PROJECTION = {
-                ContactsContract.CommonDataKinds.Email.ADDRESS,
-                ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
-        };
-
-        int ADDRESS = 0;
-        int IS_PRIMARY = 1;
-    }
-
-    /**
-     * Represents an asynchronous registration task used to authenticate
-     * the user.
-     */
 }
