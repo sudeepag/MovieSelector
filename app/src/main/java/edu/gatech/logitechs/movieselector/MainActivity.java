14d45acc63ecbcc26d9621b6baa8c73b7ae52a5b
jiangshen <jiangshen16@icloud.com>
2016-02-27 06:11:37 -0500

    changed to search view and removed toolbar

--- app/src/main/java/edu/gatech/logitechs/movieselector/MainActivity.java
+++ app/src/main/java/edu/gatech/logitechs/movieselector/MainActivity.java
@@ -1,30 +1,41 @@
 package edu.gatech.logitechs.movieselector;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.preference.PreferenceFragment;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,8 +46,13 @@ public class MainActivity extends AppCompatActivity
     UserManager manager;
     User currUser;
 
+    CoordinatorLayout mainLayout;
+    RecyclerView rv;
     TextView navHeader;
     TextView navHeaderEmail;
+
+    SearchView mSearchView;
+    MenuItem searchMenuItem;
 
     private List<Movie> movies = new ArrayList<>();
     public final RVAdapter adapter = new RVAdapter(movies);
@@ -49,11 +65,12 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mainLayout = (CoordinatorLayout) findViewById(R.id.main_layout);
 
         isAtMainPage = false;
 
         //Setting up RecyclerView
-        RecyclerView rv = (RecyclerView)findViewById(R.id.recycler_view);
+        rv = (RecyclerView)findViewById(R.id.recycler_view);
         rv.setHasFixedSize(true);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         rv.setLayoutManager(layoutManager);
@@ -61,51 +78,51 @@ public class MainActivity extends AppCompatActivity
 
         getDVD();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //get view from layout XML
-                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                final View searchFieldLayout = inflater.inflate(R.layout.search_box_view,
-                        null, false);
-                final EditText input = (EditText) searchFieldLayout.findViewById(R.id.search_field);
-                // create the AlertDialog as final
-                final AlertDialog dialog = new AlertDialog.Builder(MainActivity.this)
-                        .setTitle("Find Movie")
-                        .setView(searchFieldLayout)
-                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int id) {
-                                MovieManager.searchTitles(input.getText().toString(), MainActivity.this, new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        movies = MovieManager.getMovieList();
-                                        adapter.updateMovieList(movies);
-                                    }
-                                });
-                            }
-                        })
-                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int id) {
-                                dialog.cancel();
-                            }
-                        })
-                        .create();
-                // set the focus change listener of the EditText
-                // this part will make the soft keyboard automatically visible
-                input.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-                    @Override
-                    public void onFocusChange(View v, boolean hasFocus) {
-                        if (hasFocus) {
-                            dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
-                        }
-                    }
-                });
-                dialog.show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                //get view from layout XML
+//                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//                final View searchFieldLayout = inflater.inflate(R.layout.search_box_view,
+//                        null, false);
+//                final EditText input = (EditText) searchFieldLayout.findViewById(R.id.search_field);
+//                // create the AlertDialog as final
+//                final AlertDialog dialog = new AlertDialog.Builder(MainActivity.this)
+//                        .setTitle("Find Movie")
+//                        .setView(searchFieldLayout)
+//                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialog, int id) {
+//                                MovieManager.searchTitles(input.getText().toString(), MainActivity.this, new Runnable() {
+//                                    @Override
+//                                    public void run() {
+//                                        movies = MovieManager.getMovieList();
+//                                        adapter.updateMovieList(movies);
+//                                    }
+//                                });
+//                            }
+//                        })
+//                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialog, int id) {
+//                                dialog.cancel();
+//                            }
+//                        })
+//                        .create();
+//                // set the focus change listener of the EditText
+//                // this part will make the soft keyboard automatically visible
+//                input.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+//                    @Override
+//                    public void onFocusChange(View v, boolean hasFocus) {
+//                        if (hasFocus) {
+//                            dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+//                        }
+//                    }
+//                });
+//                dialog.show();
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -119,6 +136,29 @@ public class MainActivity extends AppCompatActivity
         navHeader = (TextView) findViewById(R.id.nav_header_text);
         navHeaderEmail = (TextView) findViewById(R.id.nav_header_email);
     }
+
+    SearchView.OnQueryTextListener searchListener = new SearchView.OnQueryTextListener() {
+        @Override
+        public boolean onQueryTextSubmit(String query) {
+            MenuItemCompat.collapseActionView(searchMenuItem);
+//            rvDisappear();
+            MovieManager.searchTitles(query, MainActivity.this, new Runnable() {
+                @Override
+                public void run() {
+                    movies = MovieManager.getMovieList();
+                    adapter.updateMovieList(movies);
+                }
+            });
+//            rvAppear();
+            return false;
+        }
+
+        @Override
+        public boolean onQueryTextChange(String newText) {
+            // newText is text entered by user to SearchView
+            return false;
+        }
+    };
 
     protected void onStop() {
 //        update navigation headers
@@ -145,6 +185,9 @@ public class MainActivity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        searchMenuItem = menu.findItem(R.id.action_search);
+        mSearchView = (SearchView) searchMenuItem.getActionView();
+        mSearchView.setOnQueryTextListener(searchListener);
         return true;
     }
 
@@ -155,14 +198,28 @@ public class MainActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        if (id == R.id.action_about) {
+        if (id == R.id.action_search) {
             return true;
-        } else if (id == R.id.action_new_release) {
-            getRecent();
-        } else if (id == R.id.action_dvd) {
-            getDVD();
+        } else if (id == R.id.action_about) {
+            Snackbar snackbar = Snackbar
+                        .make(mainLayout, "Made with ❤️ from CS 2340", Snackbar.LENGTH_LONG)
+                        .setAction("MORE INFO", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                AlertDialog dialog = new AlertDialog.Builder(MainActivity.this)
+                                    .setTitle("About us")
+                                    .setMessage("hi!")
+                                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int id) {
+                                            dialog.cancel();
+                                        }
+                                    }).create();
+                                dialog.show();
+                            }
+                        });
+            snackbar.show();
         }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -172,7 +229,11 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_manage) {
+        if (id == R.id.nav_dvd_library) {
+            getDVD();
+        } else if (id == R.id.nav_new_release) {
+            getRecent();
+        } else if (id == R.id.nav_manage) {
             Intent myIntent = new Intent(MainActivity.this, UserProfile.class);
             //DO not show headers when loading preference
             myIntent.putExtra(PreferenceActivity.EXTRA_SHOW_FRAGMENT, UserProfile.class);
@@ -189,23 +250,44 @@ public class MainActivity extends AppCompatActivity
     }
 
     private void getDVD() {
+        rvDisappear();
         MovieManager.getDVD(this, new Runnable() {
             @Override
             public void run() {
                 movies = MovieManager.getMovieList();
+//                rvAnimate(false);
                 adapter.updateMovieList(movies);
+//                rvAnimate(true);
             }
         });
+        adapter.updateMovieList(movies);
+        rvAppear();
     }
 
     private void getRecent() {
+        rvDisappear();
         MovieManager.getRecent(MainActivity.this, new Runnable() {
             @Override
             public void run() {
                 movies = MovieManager.getMovieList();
                 adapter.updateMovieList(movies);
+
             }
         });
+        adapter.updateMovieList(movies);
+        rvAppear();
+    }
+
+    private void rvAppear() {
+        AlphaAnimation anim = new AlphaAnimation(0.0f, 1.0f);
+        anim.setDuration(650);
+        rv.startAnimation(anim);
+    }
+
+    private void rvDisappear() {
+        AlphaAnimation anim = new AlphaAnimation(1.0f, 0.0f);
+        anim.setDuration(800);
+        rv.startAnimation(anim);
     }
 
     private void logoutAction() {
