0e675eedc2839f3648ad0d6d4bf7e46e09e98b66
jiangshen <jiangshen16@icloud.com>
2016-03-27 22:56:25 -0400

    changed to activity_muvee_admin

--- app/src/main/java/edu/gatech/logitechs/movieselector/MuveeAdminActivity.java
+++ app/src/main/java/edu/gatech/logitechs/movieselector/MuveeAdminActivity.java
@@ -17,7 +17,7 @@ public class MuveeAdminActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_muvee_admin);
+        setContentView(R.layout.muvee_admin_activity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
