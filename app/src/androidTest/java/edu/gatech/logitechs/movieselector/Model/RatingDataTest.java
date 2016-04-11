f9830a6d8348f11e434bedad4fcd776017ffc87d
Akhilesh Aji <akhiaji@gmail.com>
2016-04-10 20:00:22 -0400

    updated a testing file for RatingData

---
+++ app/src/androidTest/java/edu/gatech/logitechs/movieselector/Model/RatingDataTest.java
@@ -0,0 +1,57 @@
+package edu.gatech.logitechs.movieselector.Model;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by akhilesh on 4/10/16.
+ */
+public class RatingDataTest {
+    private RatingData ratingData = new RatingData("This Movie", "1234");
+
+    @Before
+    public void setUp() throws Exception {
+
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testAddRating() throws Exception {
+        HashMap<String, Double> average = new HashMap<>();
+        HashMap<String, Integer> numEntries = new HashMap<>();
+        HashMap<String, Double> sum = new HashMap<>();
+        assertEquals(average, ratingData.getAverage());
+        assertEquals(numEntries, ratingData.getNumRating());
+        assertEquals(sum, ratingData.getSum());
+        ratingData.addRating("major", 4);
+        average.put("major", 4.0);
+        numEntries.put("major", 1);
+        sum.put("major", 4.0);
+        assertEquals(average, ratingData.getAverage());
+        assertEquals(numEntries, ratingData.getNumRating());
+        assertEquals(sum, ratingData.getSum());
+        ratingData.addRating("major", 2.0);
+        average.put("major", 3.0);
+        numEntries.put("major", 1);
+        sum.put("major", 6.0);
+        assertEquals(average, ratingData.getAverage());
+        assertEquals(numEntries, ratingData.getNumRating());
+        assertEquals(sum, ratingData.getSum());
+        assertEquals(5,4);
+    }
+
+    @Test
+    public void testCalculateRating() throws Exception {
+
+    }
+}
